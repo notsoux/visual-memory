4e3dc1bd886ac85222d51de488093f514d0a3caa
notsoux <wellstaff@gmail.com>
2016-07-19 09:54:06 +0200

    Initial Commit

---
+++ visual-memory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  visual-memory
+//
+//  Created by William Pompei on 19/07/16.
+//  Copyright © 2016 William Pompei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
